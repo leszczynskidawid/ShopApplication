399765dccdb3a33b1c94f8841271fcc064e170a5
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2022-04-03 14:26:17 +0200

    Added new file ApiEndpoints

--- src/constant/Constatnt.js
+++ src/constant/Constatnt.js
@@ -1,10 +1,10 @@
-import Header from "components/organisms/Header"
-
+import { ApiEndpoints } from "./ApiEndpoints"
 
 export const ApConnect = {
     KEY: 'X6TyMgzQpaZ1zGD7CXMGwdOa6tGhv8PY', 
     SECRET:"GIfpmTr97g5fkYQb",
-    APP_ID: "a2ea9eb4-3559-486c-87e7-a33a36be1d63"
+    APP_ID: "a2ea9eb4-3559-486c-87e7-a33a36be1d63",
+    BASE_URL: "https://api.nytimes.com/"
     
 }
 
@@ -16,7 +16,7 @@ export const  fetchData = async () =>
    
 
  
-    const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${ApConnect.KEY}`)
+    const response = await fetch(`${ApConnect.BASE_URL}${ApiEndpoints.FETCH_BOOKS}?api-key=${ApConnect.KEY}`)
 
     
         const json = await response.json()
